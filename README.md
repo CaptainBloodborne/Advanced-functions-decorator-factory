e019ff2fa74d19533786fe4f80940f8372db2af0
Andrei Dvoeglazov <Andrei_Dvoeglazov@epam.com>
2021-02-26 17:11:42 +0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Advanced functions
+Create a decorator which calculates function execution time.
+Use `time` module.
---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from typing import Callable
+
+
+def apply(func: Callable):
+    """
+    Create a decorators factory (decorator itself).
+    The factory accepts a function (lambda) as an input and returns a decorator
+    that will return the result of the function as the first argument,
+    the result of the decorated function is passed.
+
+    For example:
+
+    @apply(lambda user_id: user_id + 1)
+    def return_user_id():
+        return 42
+
+    >> return_user_id()
+    will return 43
+    """
+    raise NotImplementedError('Implement me!')
